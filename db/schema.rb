ffb416ab4ed4d0af342009bc86e4460255787705
Bucinskas <jonas@bucinskas.lt>
2021-09-07 08:20:14 +0300

    Adding a user model to the test app

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_03_172932) do
+ActiveRecord::Schema.define(version: 2021_09_07_045555) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_it"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
